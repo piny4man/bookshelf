a1310fd4ff27d3e2a1d6d232a034d4076baac24d
piny4man <development@pinya.dev>
2023-12-22 18:35:40 +0100

    learn(exercise 4): auth extra credits

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -1,11 +1,13 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
-
 import * as React from 'react'
 import * as auth from 'auth-provider'
 import { AuthenticatedApp } from './authenticated-app'
 import { UnauthenticatedApp } from './unauthenticated-app'
 import { client } from './utils/api-client'
+import { useAsync } from './utils/hooks'
+import { FullPageSpinner } from './components/lib'
+import * as colors from './styles/colors'
 
 const getUser = async () => {
   let user = null
@@ -19,19 +21,37 @@ const getUser = async () => {
 }
 
 function App() {
-  const [user, setUser] = React.useState(null)
+  const { data: user, setData, run, error, isLoading, isIdle, isError } = useAsync()
 
-  const login = form => auth.login(form).then(u => setUser(u))
-  const register = form => auth.register(form).then(u => setUser(u))
+  const login = form => auth.login(form).then(u => setData(u))
+  const register = form => auth.register(form).then(u => setData(u))
   const logout = () => {
     auth.logout()
-    setUser(null)
+    setData(null)
   }
 
   React.useEffect(() => {
-    getUser().then(u => setUser(u))
-  }, [])
+    run(getUser())
+  }, [run])
 
+  if (isLoading || isIdle) return <FullPageSpinner />
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
   if (Boolean(user)) return <AuthenticatedApp user={user} logout={logout} />
   return <UnauthenticatedApp login={login} register={register} />
 }
