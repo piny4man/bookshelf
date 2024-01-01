810ef22c47f544d89fae9e907f250899773e398b
piny4man <development@pinya.dev>
2024-01-01 21:10:44 +0100

    learn(performance): exercise solution - code splitting'

--- src/authenticated-app.exercise.js
+++ src/authenticated-app.exercise.js
@@ -157,9 +157,5 @@ function AppRoutes() {
   )
 }
 
-// 🐨 change this to a default export
-export {AuthenticatedApp}
+export default AuthenticatedApp
 
-// 🐨 Unfortunately, to make this work for our workshop,
-// you need to add this to src/authenticated-app.js:
-// export {default} from './authenticated-app.exercise'
