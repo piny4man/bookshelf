db8191e57c41ae1453a021d43f816b7d6d264263
piny4man <development@pinya.dev>
2024-01-04 17:48:37 +0100

    learn(unit-testing): extra 1 and 2 solutions

--- src/utils/__tests__/api-client.exercise.js
+++ src/utils/__tests__/api-client.exercise.js
@@ -1,11 +1,12 @@
+import { queryCache } from 'react-query'
+import * as auth from 'auth-provider'
 import { server, rest } from 'test/server'
 import { client } from '../api-client'
 
 const apiURL = process.env.REACT_APP_API_URL
 
-beforeAll(() => server.listen())
-afterAll(() => server.close())
-afterEach(() => server.resetHandlers())
+jest.mock('react-query')
+jest.mock('auth-provider')
 
 test('calls fetch at the endpoint with the arguments for GET requests', async () => {
   const endpoint = 'test-endpoint'
@@ -78,3 +79,29 @@ test(
   }
 )
 
+test('correctly rejects the promise if there is an error', async () => {
+  const endpoint = 'test-endpoint'
+  const testError = { message: 'Test error' }
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      return res(ctx.status(400), ctx.json(testError))
+    })
+  )
+
+  await expect(client(endpoint)).rejects.toEqual(testError)
+})
+
+test('automatically logs the user out if a request returns a 401', async () => {
+  const endpoint = 'test-endpoint'
+  const mockResult = { mock: 'VALUE' }
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      return res(ctx.status(401), ctx.json(mockResult))
+    })
+  )
+
+  const error = await client(endpoint).catch(error => error)
+  expect(error.message).toMatchInlineSnapshot(`"Please re-authenticate."`)
+  expect(queryCache.clear).toHaveBeenCalledTimes(1)
+  expect(auth.logout).toHaveBeenCalledTimes(1)
+})
